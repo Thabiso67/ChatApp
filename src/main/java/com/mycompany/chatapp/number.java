5707b8328e51b8cf666efb6afd532fb3c57e6b22
lab_services_student <lab_services_student@10.0.0.65>
2025-04-22 10:25:23 +0200

    First Commit

---
+++ src/main/java/com/mycompany/chatapp/number.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chatapp;
+
+/**
+ *
+ * @author lab_services_student
+ */
+class number {
+
+    static boolean matches(String wazAZ26$) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
